0531866f279093de729033fcb01ae10131e14406
Kevin He <he.changbin@bugtags.com>
2015-12-16 10:31:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Embrace-Android-Studio-Demo
---
+++ s2-AsInDepth/app/src/pay/java/io/kvh/as/asindepth/DiffBean.java
@@ -0,0 +1,12 @@
+package io.kvh.as.asindepth;
+
+import android.util.Log;
+
+/**
+ * Created by bugtags.com on 15/12/15.
+ */
+public class DiffBean {
+    public void type() {
+        Log.i("DiffBean", "pay");
+    }
+}
